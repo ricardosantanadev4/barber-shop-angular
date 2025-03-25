6310dab0f71cb7087b1e1c3a7bbd597293a453f7
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-25 15:24:47 -0300

    feat: Components criados.

---
+++ src/app/dashboard/components/schedule-table/schedule-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './schedule-table.component.html',
+  styleUrl: './schedule-table.component.scss'
+})
+export class ScheduleTableComponent {
+
+}
