9a3236c6926a2ade20f4bc2ff8f789a21ad237f4
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-26 20:16:10 -0300

    feat: Configurações de navegação.

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private router: Router) {
-    this.router.navigate(['/schedule-painel']);
+
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.initForm();
   }
+
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]],
+    })
+  }
+
+  getErrorMessageFromFormField(field: string) {
+    const getField = this.loginForm.get(field);
+    switch (true) {
+      case getField?.hasError('required'):
+        return `O campo é obrigatório`;
+        case getField?.hasError('email'):
+          return `Email inválido.`;
+      default:
+        return '';
+    }
+  }
+
+  navigateTo() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/schedule-painel']);
+    }
+  }
+
 }
--- src/app/dashboard/components/header/header.component.ts
+++ src/app/dashboard/components/header/header.component.ts
@@ -1,16 +1,24 @@
 import { Component, Input } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   @Input() title = '';
+
+  constructor(private router: Router) { }
+
+  signOut() {
+    this.router.navigate(['/auth/login']);
+  }
 }
