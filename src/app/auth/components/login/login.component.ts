f7c453bebacfda4518d8aa29d0d586410b2db1d3
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-26 17:35:40 -0300

    feat: Configurações iniciais do component adicionadas.

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private router: Router){
+  constructor(private router: Router) {
     this.router.navigate(['/schedule-painel']);
   }
 }
