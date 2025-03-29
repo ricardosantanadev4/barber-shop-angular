7d896e4cacc544c7aa8109411e1eba59e7ed88c1
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-29 18:31:06 -0300

    refactor: Component ajustado.

--- src/app/dashboard/components/client-table/client-table.component.ts
+++ src/app/dashboard/components/client-table/client-table.component.ts
@@ -17,25 +17,71 @@ import { ClientService } from '../../../shared/services/client.service';
 })
 export class ClientTableComponent implements AfterViewInit {
 
-  displayedColumns: string[] = ['id', 'nome', 'email', 'telefone'];
-  dataSource: MatTableDataSource<IClient> = new MatTableDataSource<IClient>([]);
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  // displayedColumns: string[] = ['id', 'nome', 'email', 'telefone'];
+  // dataSource: MatTableDataSource<IClient> = new MatTableDataSource<IClient>([]);
+  // @ViewChild(MatPaginator) paginator!: MatPaginator;
+  // @ViewChild(MatSort) sort!: MatSort;
 
   filter: string = '';
+
+  // constructor(private clientService: ClientService) { }
+
+  // ngAfterViewInit() {
+  //   this.dataSource.paginator = this.paginator;
+  //   this.dataSource.sort = this.sort;
+
+  //   // Carregar os dados da API após a inicialização
+  //   this.loadClientsPage();
+
+  //   // Atualizar a página ao mudar o paginator ou o sort
+  //   this.paginator.page.subscribe(() => this.loadClientsPage());
+  //   this.sort.sortChange.subscribe(() => this.loadClientsPage());
+  // }
+
+  // loadClientsPage() {
+  //   const pageIndex = this.paginator.pageIndex;
+  //   const pageSize = this.paginator.pageSize;
+  //   const sortDirection = this.sort.direction;
+  //   const sortActive = this.sort.active;
+
+  //   this.clientService.listarClientesPaginados(pageIndex, pageSize, this.filter).subscribe(response => {
+  //     if (response.body) {
+  //       this.dataSource.data = response.body.content;
+  //       this.paginator.length = response.body.totalElements;
+
+  //       // Corrigindo a atualização do índice da página
+  //       this.paginator.pageIndex = response.body.number;
+  //     }
+  //   });
+  // }
+
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   console.log(filterValue);
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
+
+
+  displayedColumns: string[] = ['id', 'nome', 'email', 'telefone'];
+  clients: IClient[] = []; // Armazena os dados diretamente
+  totalElements = 0;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private clientService: ClientService) { }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-
-    // Carregar os dados da API após a inicialização
     this.loadClientsPage();
 
-    // Atualizar a página ao mudar o paginator ou o sort
     this.paginator.page.subscribe(() => this.loadClientsPage());
-    this.sort.sortChange.subscribe(() => this.loadClientsPage());
+    this.sort.sortChange.subscribe(() => {
+      this.paginator.pageIndex = 0;
+      this.loadClientsPage();
+    });
   }
 
   loadClientsPage() {
@@ -46,33 +92,12 @@ export class ClientTableComponent implements AfterViewInit {
 
     this.clientService.listarClientesPaginados(pageIndex, pageSize, this.filter).subscribe(response => {
       if (response.body) {
-        this.dataSource.data = response.body.content;
-        this.paginator.length = response.body.totalElements;
+        this.clients = response.body.content;
+        this.totalElements = response.body.totalElements;
 
-        // Habilitar/desabilitar o botão de próxima página
-        if (response.body.number === response.body.totalPages - 1) {
-          this.paginator.hasNextPage = () => false; // Não há próxima página
-        } else {
-          this.paginator.hasNextPage = () => true; // Há próxima página
-        }
-
-        // Habilitar/desabilitar o botão de voltar
-        if (response.body.number === 0) {
-          this.paginator.hasPreviousPage = () => false; // Não há página anterior
-        } else {
-          this.paginator.hasPreviousPage = () => true; // Há página anterior
-        }
+        // Ajustar o paginator corretamente
+        this.paginator.length = this.totalElements;
       }
     });
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    console.log(filterValue);
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
 }
