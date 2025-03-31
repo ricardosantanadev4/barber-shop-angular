895aa735e84a9d3afb18344691caee11006b92ab
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-31 10:02:51 -0300

    feat: Seleção de cliente configurada.

--- src/app/dashboard/components/schedule/schedule.component.ts
+++ src/app/dashboard/components/schedule/schedule.component.ts
@@ -1,11 +1,13 @@
-import { Component, model } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, model, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { IClient } from '../../../shared/models/client.interface';
+import { ClientService } from '../../../shared/services/client.service';
 
 @Component({
   selector: 'app-schedule',
@@ -16,14 +18,31 @@ import { MatSelectModule } from '@angular/material/select';
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.scss'
 })
-export class ScheduleComponent {
-  toppings = new FormControl('');
+export class ScheduleComponent implements OnInit {
 
   startTimes: string[] = ['08:00', '09:00', '10:00', '11:00', '14:00', '15:00', '16:00', '17:00'];
 
   endTimes: string[] = ['09:00', '10:00', '11:00', '15:00', '16:00', '17:00', '18:00'];
 
-  clients: string[] = ['Rod Johnson', 'Anders Heijsberg', 'Tim Berners-Lee', 'Elon Musk', 'Linus Torwalds', 'James Gosling'];
+  clients!: IClient[];
 
   selected = model<Date | null>(null);
+
+  constructor(private clientService: ClientService) {
+  }
+
+  ngOnInit(): void {
+    this.loadClients();
+  }
+
+
+  loadClients() {
+    this.clientService.listarClientesPaginados(0, 10, '').subscribe(response => {
+      if (response.body) {
+        this.clients = response.body.content;
+        console.log((`Clients: ${response.body}`));
+      }
+    })
+  }
 }
+
