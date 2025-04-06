f2eb27e42d07cd312a9bf799e7c1d159ae9f60b6
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-04-06 00:22:29 -0300

    feat: Calendário traduzido para português.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,19 +1,42 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
+import ptBr from '@angular/common/locales/pt'; // Corrigido aqui
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { getPaginatorIntl } from '../paginator-intl-pt';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
+// Formatos de data (opcional)
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+// 👉 Aqui fora do @NgModule
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [
-    { provide: MatPaginatorIntl, useValue: getPaginatorIntl() }
+    { provide: MatPaginatorIntl, useValue: getPaginatorIntl() },
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }
   ]
 })
 export class DashboardModule { }
