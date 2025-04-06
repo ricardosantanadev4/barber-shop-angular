a4074ed58bf4745539b9184a4f03e48ee1b4ebee
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-04-05 23:18:24 -0300

    feat: Filtro de data configurado.

--- src/app/dashboard/components/schedule-table/schedule-table.component.ts
+++ src/app/dashboard/components/schedule-table/schedule-table.component.ts
@@ -1,13 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { format, isValid, parse } from 'date-fns';
 import { ISchedule } from '../../../shared/models/schedule.interface';
 import { ScheduleService } from '../../../shared/services/schedule.service';
-import { FormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-schedule-table',
@@ -25,6 +26,7 @@ export class ScheduleTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  inputValue = '';
   filter = '';
   constructor(private scheduleService: ScheduleService) { }
 
@@ -44,6 +46,8 @@ export class ScheduleTableComponent implements AfterViewInit {
     const sortDirection = this.sort.direction;
     const sortActive = this.sort.active;
 
+    this.filter = this.formatDateStrToyyyyMMdd(this.inputValue);
+
     this.scheduleService.agendamentosPaginados(pageIndex, pageSize, this.filter).subscribe(response => {
       if (response.body) {
         this.shedules = response.body.content;
@@ -54,4 +58,15 @@ export class ScheduleTableComponent implements AfterViewInit {
       }
     });
   }
+
+  formatDateStrToyyyyMMdd(inputValue: string) {
+    const parseDate = parse(this.inputValue, 'dd-MM-yyyy', new Date());
+    if (isValid(parseDate) && format(parseDate, 'dd-MM-yyyy') === this.inputValue) {
+      const parseDbDate = format(parseDate, 'yyyy-MM-dd');
+      return parseDbDate;
+    } else {
+      return this.inputValue;
+    }
+  }
+
 }
